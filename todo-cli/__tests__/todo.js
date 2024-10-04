29b03bae891c163ccaf035c3e9a298a8300e7377
HS-246 <happyhusain12@gmail.com>
2024-10-04 18:04:19 +0530

    added tests using jests

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,27 @@
+const todolist = require("../todo");
+
+const { all, add, markAsComplete } = todolist();
+
+describe("First todo test suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString().split("T")[0],
+    });
+  });
+  test("should add todo", () => {
+    const todoLength = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString().split("T")[0],
+    });
+    expect(all.length).toBe(todoLength + 1);
+  });
+  test("should mark as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
